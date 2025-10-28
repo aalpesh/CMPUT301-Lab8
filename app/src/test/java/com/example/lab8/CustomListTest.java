104697fab2981b1ac0ae1a344c0fefd2c1a4f8ef
Aalpesh <aalpeshdayal@gmail.com>
2025-10-28 16:55:40 -0600

    first phase

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -6,11 +6,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 //import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
+
 public class CustomListTest {
 
+    private CustomList MockCityList() {
+        // if your CustomList constructor takes two arguments:
+        return new CustomList(null, new ArrayList<>());
+        // if it only takes one argument (most common):
+        // return new CustomList(new ArrayList<>());
+    }
 
+    @Test
+    public void addCityTest() {
+        CustomList list = MockCityList();
+        int initialSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(initialSize + 1, list.getCount());
+    }
 }
